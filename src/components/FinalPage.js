793fa9bf0dc9c6670a55dc92f43a58e1c6cb5c8c
Varada Kalaskar <kalaskar.varada@gmail.com>
2021-09-12 23:26:12 +0530

    Testimonials ReactJs code

---
+++ src/components/FinalPage.js
@@ -0,0 +1,12 @@
+import Testimonial from "./Testimonial";
+
+const FinalPage = () => {
+    return ( 
+        <div className="finalpage">
+            <h1 style={{'margin':'10vh'}}>Client Testimonials</h1>
+            <Testimonial/>
+        </div>
+     );
+}
+ 
+export default FinalPage;
---
+++ src/components/Testimonial.js
@@ -0,0 +1,51 @@
+import Carousel from 'react-bootstrap/Carousel' ;
+import ReactPlayer from 'react-player';
+import {Row, Col} from 'react-bootstrap';
+import "./Testimonial.css";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+const Testimonial = (props) => {
+    const [userDatas, setUserDatas] = useState([]);
+    useEffect(()=>{
+        axios.get('https://testimonialapi.toolcarton.com/api').then((res)=>{
+            const allTests = res.data;
+            setUserDatas(allTests);
+        });
+    },[]);
+    return ( 
+        
+        <div className="testimonial">
+            <Carousel>
+            {userDatas.map((user)=>(
+                <Carousel.Item>
+                
+                    <img style={{'height':"300px"}}  className="d-block w-100"  src={'assets/img/R.jpeg'}/> 
+                    <Carousel.Caption className="testimonialcaption">
+                    <p className="message"> <i className="fas fa-quote-left"></i>{user.message + user.lorem}<i className="fas fa-quote-right"></i></p>
+                        <Row className="user" style={{'marginTop':'5vh'}}>
+                            <Col md={2}>
+                                <img className="avtar" src={user.avatar}/>
+                            </Col>
+                            <Col md={4}>
+                                <div className="personalinfo">
+                                    <p> <bold className="name">{user.name},</bold> <small className="location" style={{'color':'black'}}>{user.location}</small>
+                                    <br></br><h6>{user.designation}</h6>
+                                    <div className="rating">Rating: {user.rating}</div></p>
+                                </div>
+                            </Col>
+                            <Col md={3}>
+                                <ReactPlayer className="audio" url={user.audio} playing={false} controls={true} height="7vh" width="30vw" />
+                            </Col>
+                        </Row>
+                    </Carousel.Caption>
+                
+                
+                </Carousel.Item>
+                ))}
+                </Carousel>
+        </div>
+        
+     );
+}
+ 
+export default Testimonial;
